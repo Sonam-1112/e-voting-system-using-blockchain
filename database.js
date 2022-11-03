bd0f353cb9c7738e3cdc908096fe994dc6e28245
vgupta <vinod.gupta@e-stonetech.com>
2022-11-03 19:30:14 +0530

    frontend way

---
+++ database.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+  host: 'localhost', // assign your host name
+  user: 'root',      //  assign your database username
+  password: '',      // assign your database password
+  database: 'aadhar' // assign database Name
+}); 
+conn.connect(function(err) {
+  if (err) 
+  {console.log(err);}
+  console.log('Database is connected successfully !');
+});
+module.exports = conn;
